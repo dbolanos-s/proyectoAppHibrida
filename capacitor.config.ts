cb9d71558ce36148c74e84bf8b5a66bf9afcf369
Doménica Bolaños <dbolanos@espol.edu.ec>
2026-08-12 10:53:54 +0000

    feat: configuracion Android con appId y permisos

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'temp',
+  appId: 'ec.edu.copol.app',
+  appName: 'COPOL',
   webDir: 'dist'
 };
 
